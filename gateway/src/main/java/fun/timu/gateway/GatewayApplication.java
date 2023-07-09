06bd0659c13cfe6ee2639a4232db4e5de6527d2f
zhengke <zhengke996@163.com>
2023-07-09 20:15:59 +0800

    feat: 新增网关模块
    Signed-off-by: zhengke <zhengke996@163.com>

---
+++ gateway/src/main/java/fun/timu/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package fun.timu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("fun.timu")
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
