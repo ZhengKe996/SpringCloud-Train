d939dcbd98647833e600b7eff6b28e5f6a301d9d
zhengke <zhengke996@163.com>
2023-07-13 14:43:51 +0800

    feat: Batch模块 初始化

---
+++ batch/src/main/java/fun/timu/train/batch/BatchApplication.java
@@ -0,0 +1,15 @@
+package fun.timu.train.batch;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("fun.timu")
+@MapperScan("fun.timu.train.*.mapper")
+@SpringBootApplication
+public class BatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+}
